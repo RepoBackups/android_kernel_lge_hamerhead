60321b54ad0ef9ccac408af9c5ecf6cefa4bfa8a
engstk <eng.stk@sapo.pt>
2014-08-02 00:34:42 +0100

    cpufreq: bluactive: initial version
    Based of Google, CAF, CM, myfluxi and franciscofranco Interactive
    
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: Paul <javelinanddart@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -399,6 +399,10 @@ extern struct cpufreq_governor cpufreq_gov_intelliactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
 extern struct cpufreq_governor cpufreq_gov_intellimm;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_intellimm)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLUACTIVE)
+extern struct cpufreq_governor cpufreq_gov_bluactive;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_bluactive)
+
 #endif
 
 
