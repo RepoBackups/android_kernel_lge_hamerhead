f3e8f6477c692e5209c8b3dfa1d4af2c29345ae6
Paul Keith <javelinanddart@gmail.com>
2014-12-12 11:27:09 -0600

    Update tun.c

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -1252,7 +1252,7 @@ static long __tun_chr_ioctl(struct file *file, unsigned int cmd,
 	struct ifreq ifr;
 	int sndbuf;
 	int vnet_hdr_sz;
-	int ret;
+//	int ret;
 
 #ifdef CONFIG_ANDROID_PARANOID_NETWORK
 	if (cmd != TUNGETIFF && !capable(CAP_NET_ADMIN)) {
